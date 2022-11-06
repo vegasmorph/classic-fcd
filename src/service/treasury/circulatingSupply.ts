01043cf4cbcdf7d654f1b8ffb1b250a3fa55c78f
Vegas <103571823+vegasmorph@users.noreply.github.com>
2022-11-06 21:07:20 +0000

    Update circulatingSupply.ts

--- src/service/treasury/circulatingSupply.ts
+++ src/service/treasury/circulatingSupply.ts
@@ -1,6 +1,7 @@
 import * as Bluebird from 'bluebird'
 import { getRepository } from 'typeorm'
 import { UnvestedEntity } from 'orm'
+import { vestedEntity } from 'orm'
 import { minus, div, plus } from 'lib/math'
 import { currencyToDenom, isActiveCurrency } from 'lib/common'
 import memoizeCache from 'lib/memoizeCache'
@@ -28,6 +29,21 @@ export async function getCirculatingSupply(input: string): Promise<string> {
     },
     take: 1
   })
+  import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity('vested')
+export default class vestedEntity {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  datetime: Date
+
+  @Column()
+  denom: string
+
+  @Column('decimal', { precision: 40, scale: 10 })
+  amount: string
 
   // Initialize circualating supply to total supply
   let circulatingSupply = totalSupply
@@ -37,6 +53,19 @@ export async function getCirculatingSupply(input: string): Promise<string> {
     circulatingSupply = minus(circulatingSupply, unvested[0].amount)
   }
 
+   // Remove vested amount
+  (vested.length) {
+    circulatingSupply = minus(circulatingSupply, vested[0].amount)
+  }                                                                                                                                                                                                                                                                                     const vested = await getRepository(vestedEntity).find({
+    where: {
+      denom
+    },
+    order: {
+      id: 'DESC'
+    },
+    take: 1
+  })     
+  
   // Special conditions for Luna
   if (denom === 'uluna') {
     // Remove Luna in community pool
